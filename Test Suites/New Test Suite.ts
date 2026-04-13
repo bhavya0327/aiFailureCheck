2bc4635a03c4948bf9a600ff3e00e79e86593ad4
bhavya0327 <bhavyanshameta@gmail.com>
2026-04-13 18:12:21 +0530

    Adds the new TS

--- Test Suites/New Test Suite.ts
+++ Test Suites/New Test Suite.ts
@@ -1,21 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>TS01</name>
+   <name>New Test Suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>10</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>8943e289-bc53-40ed-98db-5a15e8b30a7e</testSuiteGuid>
+   <testSuiteGuid>a84acfa9-8d57-410e-bbea-0d5173dcdf3e</testSuiteGuid>
    <testCaseLink>
-      <guid>23bfa060-80bc-4a37-b621-0cba755a89f4</guid>
+      <guid>76a21332-c73b-4bcf-bcf6-0ba6063152db</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/tc01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>40e77c3c-e981-4eef-abaf-eb2cbd1a0745</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc01 - Copy</testCaseId>
       <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
